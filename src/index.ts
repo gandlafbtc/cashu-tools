8d0c6badb06e66c3f1978a55b90a6bef91b0fdba
gandlaf21 <gandlaf@proton.me>
2023-02-15 11:53:07 +0100

    expose utils

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { Faucet } from "./Faucet.js";
 import { sendToLn } from "./SendToLN.js";
+import * as utils from "./utils.js";
 
-export {Faucet, sendToLn}
+
+export {Faucet, sendToLn, utils}
